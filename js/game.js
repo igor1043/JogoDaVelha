bde32513f4931b105fb5bfa5a64463a28f571766
igor vinicius <43221464+igor1043@users.noreply.github.com>
2020-12-30 13:20:35 -0300

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,94 @@
+/*
+* Author:  Igor Joaquim dos Santos Lima
+* GitHub: https://github.com/igor036
+* E-mail: igorjoaquim.pg@gmail.com
+*/
+
+var id;
+var inGame = true;
+var userSimbol = "0";
+var iaSimbol = "X";
+var board = { 
+    0: ["","",""],
+    1: ["","",""], 
+    2: ["","",""] 
+};
+var gameScore = {
+    ia:0,
+    draw:0
+};
+
+$(function(){
+    $(".game-section").click(function() {
+        if ($(this).children(0).html() == "-" && inGame) {
+            $(this).html("<h1>"+userSimbol+"</h1>");
+            id = $(this).attr("id").split("-");
+            board[id[0]][id[1]] = userSimbol;
+            setTimeout(function() {
+                merge(board,next(iaSimbol,board));
+            }, 100);
+        } else
+            alert("Ação inválida!");
+    });
+});
+
+function merge(a,b) {
+    
+    //sem próxima jogada, o jogo termina em empate!
+    if (b == null) {
+
+        alert("Empate!");
+        inGame = false;
+        $("#draw-score").html(++gameScore.draw);
+
+    } else {
+
+        let count = 0;
+
+        for (let x = 0; x  < 3; x++) {
+            for (let y = 0; y  < 3; y++) {
+            
+                if (b[x][y] != "")
+                    count++;
+
+                if (a[x][y] == "" && b[x][y] != "") {
+                    $("#row-"+x+" div")[y].children[0].innerHTML = iaSimbol;
+                    board[x][y] = iaSimbol;
+                    break;
+                }
+            }   
+        }
+
+        //verifica se a IA ganhou com está jogada!
+        setTimeout(function() {
+        
+            if (winner(board) == iaSimbol) {
+    
+                alert("Você perdeu");
+                inGame = false;
+                $("#ia-score").html(++gameScore.ia);
+    
+            } 
+        }, 100);
+    }
+}
+
+
+function reset() {
+
+    for (let i = 0; i < 3; i++) {
+        list =  $("#row-"+i+" div");
+        for (let j = 0; j < list.length; j++) 
+            list[j].children[0].innerHTML = "-";
+        
+     }
+
+     board = { 
+        0: ["","",""],
+        1: ["","",""], 
+        2: ["","",""] 
+    };
+
+    last = null;
+    inGame = true;
+}
---
+++ js/minMax.js
@@ -0,0 +1,160 @@
+/*
+* Author:  Igor Joaquim dos Santos Lima
+* GitHub: https://github.com/igor036
+* E-mail: igorjoaquim.pg@gmail.com
+*/
+
+var last = null;
+
+function clone(matriz) {
+    
+    let clone = { 0: [], 1: [], 2: [] };
+
+    for (let y = 0; y < 3; y++)
+        for (let x = 0; x < 3; x++)
+            (clone[y])[x] = matriz[y][x];
+
+    return clone;
+}
+
+function equals(a,b){
+
+    for (let x = 0; x < 3; x++)
+        for (let y = 0; y < 3; y++)
+            if (a[x][y] != b[x][y])
+                return false;
+    return true;
+}
+
+function ready(m) {
+
+    let count = 0;
+
+    for (let x = 0; x < 3; x++) {
+        for (let y = 0; y < 3; y++) {
+
+            if (count == 6)
+                break;
+
+            if (m[x][y] != "")
+                count ++;
+            
+        }
+    }
+    
+    return count == 6;
+}
+
+function podaAlfaBeta(simbol, matriz) {
+
+    let child;
+    let temp = clone(matriz);
+    let node = {
+        value: clone(matriz),
+        simb: simbol,
+        childs: [],
+        score: winner(matriz),
+        next: null
+    };
+
+    if (node.score == "E") {
+
+        for (let x = 0; x < 3; x++) {
+            for (let y = 0; y < 3; y++) {
+                  
+                if (temp[x][y] == "") {
+    
+                    child = clone(node.value);
+                    child[x][y] = simbol;
+
+                    temp[x][y] = "w";
+                    child = podaAlfaBeta(simbol == "X" ? "0" : "X", child);
+                    
+                    node.childs.push(child);
+
+                    if (child.score == simbol) {
+
+                        node.score = simbol;
+                        node.next = child;
+
+                        if (winner(child.value) == "X")
+                            x = y = 3;
+                        
+                    } else
+                        if(node.score == "E")
+                            node.score = simbol == "X" ? "0" : "X";
+                }
+            }
+        }
+    }
+
+    return node;
+}
+
+function next(simbol,board) {
+
+    if (last == null)
+        last = podaAlfaBeta(simbol, board).next;
+    else {
+        
+        for (let i = 0; i < last.childs.length; i++) {
+            if (equals(last.childs[i].value,board)) {
+                last = last.childs[i].next;
+                break;
+            }
+        }
+    }
+
+    if (last != null)
+        return last.value;
+    return null;
+}
+
+
+function vertical(m, index) {
+
+    if (m[index][0] == m[index][1] && m[index][0] == m[index][2])
+        return m[index][0];
+
+    return "E";
+}
+
+function horizontal(m, index) {
+
+    if (m[0][index] == m[1][index] && m[0][index] == m[2][index])
+        return m[0][index];
+    return "E";
+}
+
+function diagonal(m) {
+
+    if ((m[0][0] == m[1][1] && m[0][0] == m[2][2]) ||
+        (m[0][2] == m[1][1] && m[0][2] == m[2][0]))
+        return m[1][1];
+
+    return "E";
+}
+
+
+function winner(m) {
+
+    if (!ready(m)) {
+        
+        return "E";
+    }
+
+    let r;
+
+    for (let i = 0; i < 3; i++) {
+
+        r = vertical(m, i);
+        if (r != "E")
+            break;
+
+        r = horizontal(m, i);
+        if (r != "E")
+            break;
+    }
+
+    return r != "E" ? r : diagonal(m);
+}
